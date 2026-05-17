876f189009c1e04a4edd3a6bf4d85a2aa590faa9
pixelwebdevelopers <pixelwebdevelopers1@gmail.com>
2026-05-17 19:03:34 +0500

    google tag  install

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -66,6 +66,18 @@ function RootShell({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-L5H4EZCCY1"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-L5H4EZCCY1');
+            `,
+          }}
+        />
         <HeadContent />
       </head>
       <body>
